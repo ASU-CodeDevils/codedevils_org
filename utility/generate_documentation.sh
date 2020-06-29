7c864bf449f6ad5d2854800b6665f7ee55683ff3
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-06-29 02:03:37 -0400

    Changed directory build to sphinx folder

--- utility/generate_documentation.sh
+++ utility/generate_documentation.sh
@@ -3,5 +3,5 @@
 # Utility to generate sphinx documentation and reconfigure for GitHub pages.
 cd sphinx
 make html
-mv ../docs/html/* ../docs/
-rm -rf ../docs/html
+mv _build/html/* ../docs/
+rm -rf _build
