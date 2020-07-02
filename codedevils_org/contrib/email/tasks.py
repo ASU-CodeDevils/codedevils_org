d263e795a59bd1002d3a68300914d1302b1e84f5
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-07-02 02:57:46 -0400

    Flake8 updates

--- codedevils_org/contrib/email/tasks.py
+++ codedevils_org/contrib/email/tasks.py
@@ -12,10 +12,10 @@ def unblock_expired_blacklistings():
     time interval you choose to perform the task.
     """
     blacklisted_emails = BlacklistEmail.objects.filter(is_blocked=True,
-                                                       blocked_until__isnull=False, 
+                                                       blocked_until__isnull=False,
                                                        blocked_until__gte=datetime.now())
     blacklisted_domains = BlacklistDomain.objects.filter(is_blocked=True,
-                                                         blocked_until__isnull=False, 
+                                                         blocked_until__isnull=False,
                                                          blocked_until__gte=datetime.now())
     if blacklisted_emails:
         blacklisted_emails.update(is_blocked=False)
