eec54ed63045e12453caea5b626054e93117cab1
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-07-07 16:39:21 -0400

    Removed student registration

--- codedevils_org/users/views.py
+++ codedevils_org/users/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView
-from django.views.generic.edit import FormView
 
 User = get_user_model()
 
