6fc814656b16d5e1c7c88c572d9c1bc6b114ea18
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-08-27 23:13:55 -0700

    Reverted google analytics addition

--- codedevils_org/contrib/cd_url/templatetags/custom_urls.py
+++ codedevils_org/contrib/cd_url/templatetags/custom_urls.py
@@ -1,6 +1,4 @@
 from django import template
-from django.conf import settings
-from ..models import CustomUrl
 
 register = template.Library()
 
@@ -21,17 +19,3 @@ def custom_url(slug):
         return CustomUrl.objects.get(slug=slug).url
     except CustomUrl.DoesNotExist:
         return "#"
-
-
-@register.simple_tag
-def google_analytics_tracking_id():
-    """
-    Returns the Google Analytics tracking ID as defined in settings as GOOGLE_ANALYTICS_TRACKING_ID. Note that in
-    production this should always return the corresponding tracking ID, but testing locally will return None if
-    not set.
-
-    Usage:
-        {% load custom_urls %}
-        {% google_analytics_tracking_id %}
-    """
-    return getattr(settings, "GOOGLE_ANALYTICS_TRACKING_ID", None)
