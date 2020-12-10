4bd1943816bee0f12ce65e9f36236e2e8022fb9c
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-12-10 04:17:40 -0700

    STATICFILES_STORAGE default

--- config/settings/production.py
+++ config/settings/production.py
@@ -55,7 +55,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 # MEDIA
 # ------------------------------------------------------------------------------
 
