c42dd58738f369f9a4d8447312116ac58d77bc5d
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-12-20 13:20:13 -0700

    changed base template

--- config/settings/base.py
+++ config/settings/base.py
@@ -149,7 +149,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "config.middleware.RootRedirectMiddleware"
+    #"config.middleware.RootRedirectMiddleware"
 ]
 
 # STATIC
